21f201d7de3021e449096814604d51ab5cbfe8fe
Super-Thomate <tomplaindoux1@orange.fr>
2019-07-04 21:30:35 +0200

    invite_roles

--- botconfig.example.py
+++ botconfig.example.py
@@ -5,4 +5,5 @@ config = {               'tokens': {   'Instance1':'BOT_TOKEN_1'
            ,         'create_url': 'https://my_website.com/handle_invitation.php'
            , 'invitation_channel': 1234567890123456790 # Id of the channel listened
            ,    'invitation_logs': 1234567890123456790 # Id of the channel for logs
+           ,       'invite_roles': ['role_1', 'role_2']
          }
