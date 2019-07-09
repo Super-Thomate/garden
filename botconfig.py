64ea951ddcf7e0b30162cbc3246f18297286e7e8
SuperThomate <tomplaindoux1@orange.fr>
2019-07-09 13:52:52 +0000

    add absolute path

--- botconfig.py
+++ botconfig.py
@@ -5,7 +5,7 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))+'/'
 print ("dir_path : "+dir_path)
 instance = sys.argv[1]
-with open('config.json') as json_file:  
+with open(dir_path+'config.json') as json_file:  
   config = json.load(json_file)[instance]
 
 print (config)
