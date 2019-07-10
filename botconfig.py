0ac189cc6592b0db9e6f0391563f0b75bec244b0
SuperThomate <tomplaindoux1@orange.fr>
2019-07-10 07:01:37 +0000

    some help

--- botconfig.py
+++ botconfig.py
@@ -2,10 +2,13 @@ import json
 import sys
 import os
 
+# get the path to project root
 dir_path = os.path.dirname(os.path.realpath(__file__))+'/'
-print ("dir_path : "+dir_path)
+# print ("dir_path : "+dir_path)
+# Instance name
 instance = sys.argv[1]
+# Config for this instance
 with open(dir_path+'config.json') as json_file:  
   config = json.load(json_file)[instance]
 
-print (config)
+# print (config)
