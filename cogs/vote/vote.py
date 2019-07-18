bdc6a8345ae8decef15cb9ff21e38cd98e35d70b
SuperThomate <tomplaindoux1@orange.fr>
2019-07-18 16:01:11 +0000

    vote

--- cogs/vote/vote.py
+++ cogs/vote/vote.py
@@ -11,7 +11,8 @@ class Vote(commands.Cog):
     self.utils = Utils()
     self.logger = Logs(self.bot)
     self.db = Database()
-  
+
+
   @commands.command(name='createvote', aliases=['vote'])
   @commands.guild_only()
   async def create_vote(self, ctx, *args):
@@ -38,9 +39,98 @@ class Vote(commands.Cog):
     poll = await ctx.send(content=None, embed=embed)
     # insert into vote_message values ('message_id', 'channel_id', 'month', 'year', 'guild_id')
     sql = f"insert into vote_message values ('{poll.id}', '{ctx.channel.id}', '{month}', '{year}', '{guild_id}')"
-    await ctx.send (f"`{sql}`")
+    self.db.execute_order (sql, [])
+    # await ctx.send (f"`{sql}`")
     await self.logger.log('vote_log', author, ctx.message, error)
-  
+
+
+  @commands.command(name='setdescription', aliases=['setdesc', 'desc', 'sd'])
+  @commands.guild_only()
+  async def set_description(self, ctx, message_id: str = None):
+    """ Set a description for a vote message
+    """
+    await self.handle_result (ctx, message_id, "description")
+
+  @commands.command(name='setttile', aliases=['title', 'st'])
+  @commands.guild_only()
+  async def set_title(self, ctx, message_id: str = None):
+    """ Set a title for a vote message
+    """
+    await self.handle_result (ctx, message_id, "title")
+
+  async def handle_result (self, ctx, message_id, handle):
+    author = ctx.author
+    guild_id = ctx.guild.id
+    if not self.utils.has_role (author, guild_id):
+      print ("No permissions")
+      await ctx.message.add_reaction ('❌')
+      return
+    if not message_id:
+      await ctx.send ('Missing parameters MessageID')
+      await ctx.message.add_reaction ('❌')
+      await self.logger.log('vote_log', author, ctx.message, True) 
+      return
+    try:
+      message_id = int (message_id)
+    except Exception as e:
+      await ctx.send ('Parameters MessageID must be an integer')
+      await ctx.message.add_reaction ('❌')
+      await self.logger.log('vote_log', author, ctx.message, True) 
+      return
+    # valid message saved ?
+    sql = f"select channel_id from vote_message where message_id='{message_id}' and guild_id='{guild_id}'"
+    fetched = self.db.fetch_one_line (sql)
+    if not fetched:
+      ctx.send (f"MessageID {message_id} does not correspond to a vote")
+      await self.logger.log('vote_log', author, ctx.message, True)
+      await ctx.message.add_reaction ('❌')
+      return
+    # get the message
+    try:
+      vote_msg = await ctx.channel.fetch_message (message_id)
+    except Exception as e:
+      if type(e).__name__ == "NotFound":
+        await ctx.send (f"MessageID {message_id} does not correspond to a message on this channel")
+      elif type(e).__name__ == "Forbidden":
+        await ctx.send (f"Permission denied.")
+      else:
+        await ctx.send (f"Unknown error : {type(e).__name__} - {e}")
+      await self.logger.log('vote_log', author, ctx.message, True)
+      await ctx.message.add_reaction ('❌')
+      return
+    embed = vote_msg.embeds[0]
+    
+    if handle == "description":
+      ask = await ctx.send ("Entrez la nouvelle description du vote :")
+    elif handle == "title":
+      ask = await ctx.send ("Entrez le nouveau titre du vote :")
+      
+    def check(m):
+      return m.channel == ctx.channel and m.author == ctx.author
+    msg = await self.bot.wait_for('message', check=check)
+    
+    if handle == "description":
+      embed.description = msg.content
+    elif handle == "title":
+      embed.title = msg.content
+    
+    await vote_msg.edit (embed=embed)
+    await ctx.message.add_reaction ('✅')
+    await self.logger.log('vote_log', author, ctx.message, False)
+    await self.logger.log('vote_log', author, msg, False)
+    await ask.delete(delay=0.5)
+    await msg.delete(delay=0.5)
+    await ctx.message.delete(delay=0.5)
+
+
+  @commands.command(name='addproposition', aliases=['add'])
+  @commands.guild_only()
+  async def add_proposition(self, ctx, message_id: str = None):
+    """ Set a title for a vote message
+    """
+    await self.handle_result (ctx, message_id, "title")
+
+
   def create_embed(self, reason, description, month, year):
     colour = discord.Colour(0)
     colour = colour.from_rgb(231, 184, 255)
@@ -50,7 +140,8 @@ class Vote(commands.Cog):
     embed.description = description
     embed.set_footer(text=f"{month}/{year}")
     return embed
-    
+
+
 """
 cursor.execute('CREATE TABLE IF NOT EXISTS `vote_propositions` (`proposition` VARCHAR(512) NOT NULL,`emoji` VARCHAR(256) NOT NULL, `month` VARCHAR(2) NOT NULL, `year` VARCHAR(4) NOT NULL, `author_id` VARCHAR(256) NOT NULL, `message_id` VARCHAR(256) NOT NULL, `guild_id` VARCHAR(256) NOT NULL, PRIMARY KEY (`author_id`, `month`, `year`)) ;')
 cursor.execute('CREATE TABLE IF NOT EXISTS `vote_colors` (`color` VARCHAR(6) NOT NULL,`emoji` VARCHAR(256) NOT NULL, `month` VARCHAR(2) NOT NULL, `year` VARCHAR(4) NOT NULL, `author_id` VARCHAR(256) NOT NULL, `message_id` VARCHAR(256) NOT NULL, `guild_id` VARCHAR(256) NOT NULL, PRIMARY KEY (`author_id`, `month`, `year`)) ;')
