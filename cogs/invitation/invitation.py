8f58ca6c317ac248e76682e65c4db4f3369fbed1
SuperThomate <tomplaindoux1@orange.fr>
2019-09-26 13:59:22 +0000

    master

--- cogs/invitation/invitation.py
+++ cogs/invitation/invitation.py
@@ -207,7 +207,7 @@ class Invitation(commands.Cog):
             duree            = math.floor ((last_timestamp + invite_delay) - time.time())
             print (f"duree: {duree}")
             if duree > 0:
-              await self.logger.log_dm('invite_log', member, guild, message, True)
+              await self.logger.log_dm('invite_log', member, message, guild, True)
               await message.add_reaction('❌')
               feedback       = await message.channel.send(f"Vous avez déjà demandé une invitation récemment.\nIl vous faut attendre encore {self.utils.format_time(duree)}")
               await self.logger.log_dm('invite_log', self.bot.user, feedback, guild, True)
