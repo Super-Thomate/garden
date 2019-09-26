95e1745aa9388c9c3c033cb0082754bb98a4620e
SuperThomate <tomplaindoux1@orange.fr>
2019-09-26 15:02:44 +0000

    master

--- DeleteBranch.sh
+++ DeleteBranch.sh
@@ -5,6 +5,10 @@ if [ "a$1" != "a" -a "a$1" != "amaster" -a "a$1" != "a$branch" ] ; then
   git push origin --delete $1;
   git fetch --all --prune
   git branch -D $1;
+elif [ "a$1" = "amaster" ] ; then
+  echo "Cannot delete branch master !"
+elif [ "a$1" = "a$branch" ] ; then
+  echo "Cannot delete current branch ($branch). Use 'git checkout master' first."
 else
-  echo "Cannot delete branch '$1'"
+  echo "Cannot delete empty branch"
 fi
