6389737d4fdf189e08fc54a1d9a56a390b6fc3f7
tblaudez <tblaudez@student.42.fr>
2019-10-03 19:54:11 +0200

    added !rule command

--- cogs/moderation/moderation.py
+++ cogs/moderation/moderation.py
@@ -1,18 +1,17 @@
 from discord.ext import commands
-
 from ..logs import Logs
+import Utils
 
 
 class Moderation(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
-
     self.logger = Logs(self.bot)
 
 
   @commands.Cog.listener('on_message')
   async def all_caps_emoji(self, message):
-    if (message.guild == None):
+    if (message.guild is None):
       return
     if (message.author.id == self.bot.user.id):
       return
@@ -22,7 +21,7 @@ class Moderation(commands.Cog):
 
   @commands.Cog.listener('on_message_edit')
   async def all_caps_emoji_edit(self, before, after):
-    if (before.guild == None) or (after.guild == None):
+    if (before.guild is None) or (after.guild is None):
       return
     if (before.author.id == self.bot.user.id):
       return
@@ -33,3 +32,14 @@ class Moderation(commands.Cog):
        ):
       await after.remove_reaction ("<:CapsLock:621629196359303168>", self.bot.user)
     return
+
+  @commands.command(name='rule')
+  @Utils.require(required=['authorized', 'not_banned'])
+  async def display_rule(self, ctx, rule_number: str = None):
+    if not rule_number:
+      await ctx.send(Utils.get_text(ctx.guild.id, "parameter_is_mandatory").format('<rule_index>'))
+      return
+    if not (1 <= int(rule_number) <= 10):
+      await ctx.send(Utils.get_text(ctx.guild.id, "bad_rule_index"))
+      return
+    await ctx.send(Utils.get_text(ctx.guild.id, f"rule{rule_number}"))
