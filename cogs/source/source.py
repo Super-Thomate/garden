51c4da8fb8f63ce12f32084c86eb991f993f31f8
Shampu <tblaudez@student.42.fr>
2020-01-28 18:14:10 +0100

    first shot of source

--- cogs/source/source.py
+++ cogs/source/source.py
@@ -1,5 +1,5 @@
 import random
-import re
+from datetime import datetime
 
 import discord
 import tldextract
@@ -19,9 +19,10 @@ class Source(commands.Cog):
   @Utils.require(required=['authorized', 'not_banned', 'cog_loaded'])
   async def add_source_channel(self, ctx, channel_id: int = None):
     channel_id = channel_id or ctx.channel.id
-    sql = f"INSERT INTO source_channel VALUES ('{channel_id}', '{ctx.guild.id}') ;"
+    sql = f"INSERT INTO source_channel VALUES (?, ?) ;"
+    print(sql)
     try:
-      database.execute_order(sql, [])
+      database.execute_order(sql, [channel_id, ctx.guild.id])
     except Exception as e:
       await ctx.send(Utils.get_text(ctx.guild.id, 'error_database_writing'))
       print(f"{type(e).__name__} - {e}")
@@ -31,9 +32,10 @@ class Source(commands.Cog):
   @Utils.require(required=['authorized', 'not_banned', 'cog_loaded'])
   async def remove_source_channel(self, ctx, channel_id: int = None):
     channel_id = channel_id or ctx.channel.id
-    sql = f"DELETE FROM source_channel WHERE channel_id='{channel_id}' and guild_id='{ctx.guild.id}';"
+    sql = f"DELETE FROM source_channel WHERE channel_id=? and guild_id=? ;"
+    print(sql)
     try:
-      database.execute_order(sql, [])
+      database.execute_order(sql, [channel_id, ctx.guild.id])
     except Exception as e:
       await ctx.send(Utils.get_text(ctx.guild.id, 'error_database_writing'))
       print(f"{type(e).__name__} - {e}")
@@ -45,15 +47,15 @@ class Source(commands.Cog):
     await ctx.send(Utils.get_text(ctx.guild.id, "ask_message"))
     msg = await self.bot.wait_for('message', check=lambda m: m.channel == ctx.channel and m.author == ctx.author)
     message = msg.content
-    sql = f"SELECT message FROM source_message WHERE guild_id='{ctx.guild.id}';"
-    prev_source_message = database.fetch_one_line(sql)
+    sql = f"SELECT message FROM source_message WHERE guild_id=? ;"
+    prev_source_message = database.fetch_one_line(sql, [ctx.guild.id])
     if not prev_source_message:
-      sql = f"INSERT INTO source_message VALUES (?, '{ctx.guild.id}') ;"
+      sql = f"INSERT INTO source_message VALUES (?, ?) ;"
     else:
-      sql = f"UPDATE source_message SET message=? WHERE guild_id='{ctx.guild.id}';"
+      sql = f"UPDATE source_message SET message=? WHERE guild_id=? ;"
     print(sql)
     try:
-      database.execute_order(sql, [message])
+      database.execute_order(sql, [message, ctx.guild.id])
       await ctx.message.add_reaction('✅')
     except Exception as e:
       print(f"{type(e).__name__} - {e}")
@@ -64,11 +66,12 @@ class Source(commands.Cog):
   @commands.command(name='listsourcechannels', aliases=['lsc'])
   @Utils.require(required=['authorized', 'not_banned', 'cog_loaded'])
   async def list_source_channels(self, ctx):
-    message = ""
-    sql = f"SELECT channel_id FROM source_channel WHERE guild_id='{ctx.guild.id}'"
-    channels = [channel[0] for channel in database.fetch_all_line(sql)]  # get the list of the channels' id
+    sql = f"SELECT channel_id FROM source_channel WHERE guild_id=? ;"
+    response = database.fetch_all_line(sql, [ctx.guild.id])
+    channels = [channel[0] for channel in response]
     if len(channels) == 0:
       await ctx.send(Utils.get_text(ctx.guild.id, "source_channel_list_empty"))
+    message = ""
     for channel in channels:
       message += f"- **<#{channel}>**\n"
     await ctx.send(message)
@@ -82,20 +85,21 @@ class Source(commands.Cog):
     if "." in domain:
       await ctx.send(Utils.get_text(ctx.guild.id, "source_wrong_format"))
       return
-    sql = f"SELECT domain FROM source_domain WHERE guild_id='{ctx.guild.id}' ;"
-    if domain in [domain[0] for domain in database.fetch_all_line(sql)]:
+    sql = f"SELECT domain FROM source_domain WHERE guild_id=? ;"
+    response = database.fetch_all_line(sql, [ctx.guild.id])
+    if domain in [domain[0] for domain in response]:
       await ctx.send(Utils.get_text(ctx.guild.id, "source_domain_already_blacklisted").format(domain))
       await ctx.message.add_reaction('❌')
       return
-    sql = f"INSERT INTO source_domain VALUES ('{domain}', '{ctx.guild.id}') ;"
+    sql = f"INSERT INTO source_domain VALUES (?, ?) ;"
+    print(sql)
     try:
-      database.execute_order(sql, [])
+      database.execute_order(sql, [domain, ctx.guild.id])
       await ctx.message.add_reaction('✅')
     except Exception as e:
       print(f"{type(e).__name__} - {e}")
       await ctx.message.add_reaction('❌')
       return
-    await ctx.send(Utils.get_text(ctx.guild.id, "source_domain_blacklisted").format(domain))
 
   @commands.command(name='removedomain', aliases=['rmd'])
   @Utils.require(required=['authorized', 'not_banned', 'cog_loaded'])
@@ -103,36 +107,98 @@ class Source(commands.Cog):
     if not domain:
       await ctx.send(Utils.get_text(ctx.guild.id, "error_no_parameter").format("domain"))
       return
-    sql = f"SELECT domain FROM source_domain WHERE guild_id='{ctx.guild.id}' ;"
-    if domain not in [domain[0] for domain in database.fetch_all_line(sql)]:
+    sql = f"SELECT domain FROM source_domain WHERE guild_id=? ;"
+    response = database.fetch_all_line(sql, [ctx.guild.id])
+    if domain not in [domain[0] for domain in response]:
       await ctx.send(Utils.get_text(ctx.guild.id, "source_domain_not_found").format(domain))
       await ctx.message.add_reaction('❌')
       return
-    sql = f"DELETE FROM source_domain WHERE domain='{domain}' and guild_id='{ctx.guild.id}' ;"
+    sql = f"DELETE FROM source_domain WHERE domain=? and guild_id=? ;"
+    print(sql)
     try:
-      database.execute_order(sql, [])
+      database.execute_order(sql, [domain, ctx.guild.id])
       await ctx.message.add_reaction('✅')
     except Exception as e:
       print(f"{type(e).__name__} - {e}")
       await ctx.message.add_reaction('❌')
       return
-    await ctx.send(Utils.get_text(ctx.guild.id, "source_domain_removed").format(domain))
 
   @commands.command(name='listdomains', aliases=['lsd'])
   @Utils.require(required=['authorized', 'not_banned', 'cog_loaded'])
   async def list_domains(self, ctx):
-    message = ""
-    sql = f"SELECT domain FROM source_domain WHERE guild_id='{ctx.guild.id}'"
-    domains = [domain[0] for domain in database.fetch_all_line(sql)]  # get the list of the domain
+    sql = "SELECT domain FROM source_domain WHERE guild_id=? ;"
+    response = database.fetch_all_line(sql, [ctx.guild.id])
+    domains = [domain[0] for domain in response]
     if len(domains) == 0:
       await ctx.send(Utils.get_text(ctx.guild.id, "source_no_blacklist"))
+      return
+    message = ""
     for domain in domains:
       message += f"- **{domain}**\n"
     await ctx.send(message)
 
+  @commands.command(name='addsourcepattern', aliases=['asp'])
+  @Utils.require(required=['authorized', 'not_banned', 'cog_loaded'])
+  async def add_source_pattern(self, ctx: commands.Context, kind: str, *, pattern: str):
+    """if kind.lower() not in ('y', 'n'):
+      await ctx.send(Utils.get_text(ctx.guild.id, "source_no_kind"))
+      return
+    if not pattern:
+      await ctx.send(Utils.get_text(ctx.guild.id, "error_no_parameter").format('<pattern>'))
+      return"""
+    table = "source_source_pattern" if kind.lower() == 'y' else "source_source_no_pattern"
+    sql = f"SELECT pattern FROM {table} WHERE pattern=? AND guild_id=? ;"
+    response = database.fetch_all_line(sql, [pattern, ctx.guild.id])
+    if pattern in [pattern[0] for pattern in response]:
+      await ctx.send(Utils.get_text(ctx.guild.id, "already_in_database").format('<pattern>'))
+      return
+    sql = f"INSERT INTO {table} VALUES (?, ?) ;"
+    print(sql)
+    try:
+      database.execute_order(sql, [pattern, ctx.guild.id])
+      await ctx.message.add_reaction('✅')
+    except Exception as e:
+      print(f"{type(e).__name__} - {e}")
+      await ctx.message.add_reaction('❌')
+      return
+
+  def get_list(self, table: str, guild_id: int):
+    sql = f"SELECT rowid, pattern FROM {table} WHERE guild_id=? ;"
+    response = database.fetch_all_line(sql, [guild_id])
+    message = ""
+    for pattern in response:
+      message += f"{pattern[0]} - {pattern[1]}\n"
+    return message
+
+  @commands.command(name='listsourcepattern', aliases=['lsp'])
+  @Utils.require(required=['authorized', 'not_banned', 'cog_loaded'])
+  async def list_source_pattern(self, ctx: commands.Context):
+    source_message = self.get_list('source_source_pattern', ctx.guild.id)
+    no_source_message = self.get_list('source_source_no_pattern', ctx.guild.id)
+    embed = discord.Embed(colour=discord.Color(0).from_rgb(176, 255 ,176), title="Source patterns")
+    if not source_message and not no_source_message:
+      embed.description = Utils.get_text(ctx.guild.id, "source_no_pattern")
+    if source_message:
+      embed.add_field(name=Utils.get_text(ctx.guild.id, "source_source_pattern"),
+                      value=source_message,
+                      inline=False)
+    if no_source_message:
+      embed.add_field(name=Utils.get_text(ctx.guild.id, "source_not_source_pattern"),
+                      value=no_source_message,
+                      inline=False)
+    embed.set_author(icon_url=self.bot.user.avatar_url, name=self.bot.user.name)
+    embed.timestamp = datetime.today()
+    await ctx.send(content=None, embed=embed)
+
+
+  @commands.command(name='removesourcepattern', aliases=['rsp'])
+  @Utils.require(required=['authorized', 'not_banned', 'cog_loaded'])
+  async def remove_source_pattern(self, ctx: commands.Context, kind: str, id: str):
+    sql = "DELETE"
+
   def get_source_message(self, member_id, guild_id):
-    select = f"SELECT message FROM source_message WHERE guild_id='{guild_id}';"
-    fetched = database.fetch_one_line(select)
+    select = f"SELECT message FROM source_message WHERE guild_id=? ;"
+    fetched = database.fetch_one_line(select, [guild_id])
     if not fetched:
       raise RuntimeError('source message is not set !')
     text = ""
@@ -149,21 +215,11 @@ class Source(commands.Cog):
   def is_url_in_blacklist(self, url: str, guild_id: str):
     if not Utils.is_valid_url(url):
       return False
-    sql = f"SELECT domain FROM source_domain WHERE guild_id='{guild_id}' ;"
+    sql = f"SELECT domain FROM source_domain WHERE guild_id=? ;"
+    response = database.fetch_all_line(sql, [guild_id])
     ext = tldextract.extract(url)
-    return ext.domain in [domain[0] for domain in database.fetch_all_line(sql)]
+    return ext.domain in [domain[0] for domain in response]
 
   @commands.Cog.listener('on_message')
   async def ask_for_source(self, message: discord.Message):
-    url = re.search("(?P<url>https?://[^\s]+)", message.content)
-    url = url.group("url") if url else None
-    if "source" in message.content.lower():
-      return
-    if len(message.attachments) == 0 and (
-            not url or not (self.is_url_in_blacklist(url, message.guild.id or Utils.is_url_image(url)))):
-      return
-    sql = f"SELECT channel_id FROM source_channel WHERE guild_id='{message.guild.id}' ;"
-    if not str(message.channel.id) in [channel[0] for channel in database.fetch_all_line(sql)]:
-      return
-    response = self.get_source_message(message.author.id, message.guild.id)
-    await message.channel.send(response)
+    pass
