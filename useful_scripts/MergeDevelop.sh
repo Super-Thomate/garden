ddb7e5068be5e2eaafe992257a1b349d4295521d
Super-Thomate <tomplaindoux1@orange.fr>
2020-12-07 09:57:37 +0000

    KISS

--- useful_scripts/MergeDevelop.sh
+++ useful_scripts/MergeDevelop.sh
@@ -11,15 +11,13 @@ VP_WHITE='\033[01;37m'
 VP_BOLD='\033[1m'
 VP_UNDERLINE='\033[4m'
 
-PatchToMerge="develop"
-Branch="master"
 
 git pull;
-git checkout $PatchToMerge;
+git checkout develop;
 git pull;
-git checkout $Branch;
+git checkout master;
 git pull;
-git merge $PatchToMerge 2>/tmp/Error_merge 1>/tmp/Log_merge ;
+git merge develop 2>/tmp/Error_merge 1>/tmp/Log_merge ;
 Res=$?
 if [ a"$Res" != a0 ] ; then
   Error=`cat /tmp/Error_merge`
@@ -27,23 +25,23 @@ if [ a"$Res" != a0 ] ; then
   echo -e "$Error"
   echo -e "<====== Git Merge${VP_NONE}"
   echo -e "${VP_RED}Error during merge. Fix conflict then excute:${VP_NONE}"
-  echo -e "${VP_YELLOW}git commit -am \"merge/$PatchToMerge\";"
+  echo -e "${VP_YELLOW}git commit -am \"merge/develop\";"
   echo -e "git push;"
-  echo -e "git push origin --delete $PatchToMerge;"
+  echo -e "git push origin --delete develop;"
   echo -e "git fetch --all --prune ;"
-  echo -e "git branch -D $PatchToMerge;"
-  echo -e "git checkout -b $PatchToMerge master;"
-  echo -e "git push origin $PatchToMerge;"
-  echo -e "git push --set-upstream origin $PatchToMerge;${VP_NONE}"
+  echo -e "git branch -D develop;"
+  echo -e "git checkout -b develop master;"
+  echo -e "git push origin develop;"
+  echo -e "git push --set-upstream origin develop;${VP_NONE}"
 else
-  git commit -am "merge/$PatchToMerge";
+  git commit -am "merge/develop";
   git push;
-  git push origin --delete $PatchToMerge;
+  git push origin --delete develop;
   git fetch --all --prune ;
-  git branch -D $PatchToMerge;
-  git checkout -b $PatchToMerge master;
-  git push origin $PatchToMerge;
-  git push --set-upstream origin $PatchToMerge;
+  git branch -D develop;
+  git checkout -b develop master;
+  git push origin develop;
+  git push --set-upstream origin develop;
 fi
 rm /tmp/Error_merge
 rm /tmp/Log_merge
